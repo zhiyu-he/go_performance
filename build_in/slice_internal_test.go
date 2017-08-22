99e502607d9ae5cff0239c2631e6cd9abb47fd80
hezhiyu <hezhiyu@bytedance.com>
2017-08-22 15:19:37 +0800

    [go, slice] ADD: add slice_internal test code1
    Change-Id: I7ee04eb79bb0a364962f588eaa56d278dc6e5d6b

--- build_in/slice_internal_test.go
+++ build_in/slice_internal_test.go
@@ -1,9 +1,10 @@
 package build_in
 
 import (
-	"unsafe"
-	"testing"
 	"encoding/binary"
+	"testing"
+	"unsafe"
+	//"fmt"
 )
 
 type OurSlice struct {
@@ -11,7 +12,6 @@ type OurSlice struct {
 	len int
 	cap int
 }
-
 
 const (
 	length_int32 = 4
@@ -23,7 +23,6 @@ var local_int_length = int(unsafe.Sizeof(int(0)))
 func TestLocalIntLength(t *testing.T) {
 	t.Logf("LocalIntLength: %v\n", local_int_length)
 }
-
 
 func TestGoSliceMemLayout(t *testing.T) {
 	s := make([]int64, 0, 20)
@@ -40,9 +39,9 @@ func TestGoSliceMemLayout(t *testing.T) {
 	s = append(s, 119)
 	s = append(s, 120)
 	/*
-	s[0] = 110
-	s[1] = 119
-	s[2] = 120
+		s[0] = 110
+		s[1] = 119
+		s[2] = 120
 	*/
 
 	ourSlice := (*OurSlice)(unsafe.Pointer(&s))
@@ -51,15 +50,85 @@ func TestGoSliceMemLayout(t *testing.T) {
 	t.Logf("our-slice-cap: %v\n", ourSlice.cap)
 
 	/*
-	tmp := &OurSlice{
-		ptr: ourSlice.ptr,
-		len: ourSlice.len,
-		cap: ourSlice.len,
-	}
+		tmp := &OurSlice{
+			ptr: ourSlice.ptr,
+			len: ourSlice.len,
+			cap: ourSlice.len,
+		}
 	*/
 
 	t.Logf("our-slice-values: %v\n", *(*[]int64)(unsafe.Pointer(ourSlice)))
 }
 
+var commonPool = make([]interface{}, 0, 8192)
 
+var commonPoolAddr = &commonPool
 
+func TestSlicePool(t *testing.T) {
+	commonPool := make([]interface{}, 0, 1000)
+	commonPoolAddr := (*[]interface{})(unsafe.Pointer(&commonPool))
+
+	realSlice := (*[]int64)(unsafe.Pointer(commonPoolAddr))
+	t.Logf("common-pool-addr: %v\n", unsafe.Pointer(&commonPool))
+	t.Logf("real-slice-addr: %v\n", unsafe.Pointer(realSlice))
+
+	*realSlice = append(*realSlice, 1)
+	*realSlice = append(*realSlice, 2)
+	*realSlice = append(*realSlice, 3)
+
+	t.Logf("%#v\n", *realSlice)
+}
+
+type SimpleObject struct {
+	Index int64
+}
+
+func GetSlice() *[]interface{} {
+	return commonPoolAddr
+}
+
+func ResetSlice() {
+	*commonPoolAddr = (*commonPoolAddr)[:0]
+}
+
+func doWithPool() {
+
+	//fmt.Printf("before: len: %d cap: %d\n", len(commonPool), cap(commonPool))
+	//realSlice := (*[]*SimpleObject)(unsafe.Pointer(GetSlice()))
+	_ = (*[]*SimpleObject)(unsafe.Pointer(GetSlice()))
+
+	/*
+		for i := 0; i < 5000; i++ {
+			*realSlice = append(*realSlice, &SimpleObject{Index: int64(i + 1)})
+		}
+	*/
+	//fmt.Printf("in-process: len: %d cap: %d\n", len(commonPool), cap(commonPool))
+	ResetSlice()
+	//fmt.Printf("after: len: %d cap: %d\n", len(commonPool), cap(commonPool))
+}
+
+func doWithNormal() {
+	_ = make([]*SimpleObject, 0, 8192)
+	/*
+		for i := 0; i < 5000; i++ {
+			slice = append(slice, &SimpleObject{Index: int64(i + 1)})
+		}
+	*/
+}
+
+func TestSliceReuse(t *testing.T) {
+	doWithPool()
+	doWithPool()
+}
+
+func BenchmarkTestSliceReuse(b *testing.B) {
+	for i := 0; i <= b.N; i++ {
+		doWithPool()
+	}
+}
+
+func BenchmarkTestSliceNormal(b *testing.B) {
+	for i := 0; i <= b.N; i++ {
+		doWithNormal()
+	}
+}
