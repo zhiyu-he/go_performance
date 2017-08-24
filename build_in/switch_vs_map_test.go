e0ae76d8d198babc5f680aba6702ca3f92a50947
hezhiyu <hezhiyu@bytedance.com>
2017-08-24 21:10:01 +0800

    upgrade switch_vs_map_test.go
    Change-Id: I0d6f8d9624ee400989d453ea92c044b155c705c9

--- build_in/switch_vs_map_test.go
+++ build_in/switch_vs_map_test.go
@@ -2,6 +2,9 @@ package build_in
 
 import "testing"
 
+var s1 = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+var s2 = []string{"abc", "efg", "hij", "klm"}
+
 func IsExceptedValueSwitch(val int) bool {
 	switch val {
 	case
@@ -14,7 +17,13 @@ func IsExceptedValueSwitch(val int) bool {
 		7,
 		8,
 		9,
-		10:
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16:
 		return true
 	default:
 		return false
@@ -25,23 +34,40 @@ func IsExceptedValueSwitch2(val string) bool {
 	switch val {
 	case
 		"abc",
-		"efg":
+		"efg",
+		"hij",
+		"klm":
 		return true
 	default:
 		return false
 	}
 }
 
-
 var m = map[int]bool{
-	1: true,
+	1:  true,
+	2:  true,
+	3:  true,
+	4:  true,
+	5:  true,
+	6:  true,
+	7:  true,
+	8:  true,
+	9:  true,
+	10: true,
+	11: true,
+	12: true,
+	13: true,
+	14: true,
+	15: true,
+	16: true,
 }
 
 var m2 = map[string]bool{
 	"abc": true,
 	"efg": true,
+	"hij": true,
+	"klm": true,
 }
-
 
 func IsExceptedValueMap(val int) bool {
 	return m[val]
@@ -51,6 +77,24 @@ func IsExceptedValueMap2(val string) bool {
 	return m2[val]
 }
 
+func IsExceptedValueSlice(val int) bool {
+	for _, item := range s1 {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
+
+func IsExceptedValueSlice2(val string) bool {
+	for _, item := range s2 {
+		if item == val {
+			return true
+		}
+	}
+	return false
+
+}
 
 func BenchmarkSwitch(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -62,6 +106,19 @@ func BenchmarkMap(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		IsExceptedValueMap(1)
 	}
+}
+
+func BenchmarkSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsExceptedValueSlice(1)
+	}
+}
+
+func BenchmarkSlice2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsExceptedValueSlice2("abc")
+	}
+
 }
 
 func BenchmarkSwitch2(b *testing.B) {
