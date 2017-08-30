1fde45fe757019f8d9ac452ba8563ed130fde1bc
hezhiyu <hezhiyu@bytedance.com>
2017-08-31 01:33:36 +0800

    http-cli keep-alive test
    Change-Id: I326be92ed19b407f370994aa30611eb157d693c0

---
+++ lib/http_long_connction/http_cli.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"net/http"
+//	"io/ioutil"
+	"fmt"
+	"net"
+	"time"
+	"io/ioutil"
+)
+
+const URL = "http://127.0.0.1:4102"
+
+
+
+func doPost(client *http.Client, url, topic string) (string, error) {
+	uri := url + "/put?topic=" + topic
+
+	fmt.Printf("URL: %v\n", uri)
+	resp, err := client.Post(uri, "plain/text", nil)
+	if err != nil {
+		return "", err
+	}
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+	return string(bodyBytes), nil
+
+	err = resp.Body.Close()
+	return "", err
+}
+
+func doPost2(client *http.Client, url, topic string) (string, error) {
+	uri := url + "/put?topic=" + topic
+
+	fmt.Printf("URL: %v\n", uri)
+	resp, err := client.Post(uri, "plain/text", nil)
+	if err != nil {
+		return "", err
+	}
+
+	err = resp.Body.Close()
+	return "", err
+}
+
+
+func main() {
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			Dial: func(network, addr string) (net.Conn, error) {
+				conn, err  := net.DialTimeout(network, addr, 50 * time.Millisecond)
+				fmt.Printf("LocalAddr: %v RemoteAddr: %v\n", conn.LocalAddr(), conn.RemoteAddr())
+				return conn, err
+			},
+			MaxIdleConnsPerHost: 2, // 单机可以保持10个长链接, 默认为2个
+		},
+	}
+	// 循环中没有Connection的创建, 说明进行了复用, test-case1
+	/*
+	for {
+		go doPost(client, URL, "mq_ad1")
+		go doPost(client, URL, "mq_ad2")
+		time.Sleep(2 * time.Second)
+	}
+	*/
+
+	// 由于server返回了msg, 所以这个消息cli不读的情况下, 就重新创建链接 test-case2
+	for {
+		go doPost2(client, URL, "mq_ad1")
+		go doPost2(client, URL, "mq_ad2")
+		time.Sleep(2 * time.Second)
+	}
+
+
+}
---
+++ lib/http_long_connction/http_server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"io"
+	"fmt"
+)
+
+func HandlePut(w http.ResponseWriter, req *http.Request) {
+	fmt.Printf("Here: %v\n", req.RequestURI)
+	io.WriteString(w, "something-else")
+}
+
+func main() {
+	http.HandleFunc("/put", HandlePut)
+
+	err := http.ListenAndServe("127.0.0.1:4102", nil)
+	if err != nil {
+		panic(err)
+	}
+}
